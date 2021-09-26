8c3ea418d13a9bb621155b0d3267608cbff68c80
jpucha <lilijen30@gmail.com>
2021-09-26 10:38:53 -0500

    solvented conflicts  2

--- ReservaCine/src/ec/its/reserva/cine/com/Reserva.java
+++ ReservaCine/src/ec/its/reserva/cine/com/Reserva.java
@@ -20,50 +20,44 @@ public class Reserva {
 		return fechaReserva;
 	}
 
-<<<<<<< HEAD
-    public void setFechaReserva(String fechaReserva) {
-        this.fechaReserva = fechaReserva;
-    }
-=======
-  public void setFechaReserva(String fechaReserva) {
-    this.fechaReserva = fechaReserva;
-  }
->>>>>>> 51c398e5e31a2a8090020e6b1a64ba8fc50bf2a8
-    
-  public static Reserva asignarValoresReservasAfiliado(){
-    Reserva reserve= new Reserva();
-    reserve.setCodigoReserva(id++);
-    reserve.setFechaReserva(new Date().toString());
-    return reserve;
-  }
-    
-  public Reserva asignarValores()
-  {
-    Reserva reservaNormal = new Reserva ();
-    //reservaNormal.setReservaN("1123");
-    reservaNormal.setFechaReserva("1/08/2021");
+	public void setFechaReserva(String fechaReserva) {
+		this.fechaReserva = fechaReserva;
+	}
 
-    return reservaNormal;
-  }
-    
-    public void imprimirReservaClienteNormal(Reserva reserve, ClienteNormal clienteNormal, Ticket ticket, Sala sala, Pelicula pelicula){
-        System.out.println("Reserva Cine");
-        System.out.println("Cliente Nornmal");
-        //System.out.println("RESERVA NO. "+reserve.getReservaN());
-        System.out.println("Nombre Cliente: "+ clienteNormal.getNombre());
-        System.out.println("Apellido: "+ clienteNormal.getApellido());
-        System.out.println("Edad: "+clienteNormal.getEdad() );
-        System.out.println("Precio Normal: "+ clienteNormal.getPrecionormal());
-        System.out.println("Pelicula: "+ pelicula.getTitulo());
-        System.out.println("Genero: "+ pelicula.getGenero());
-        System.out.println("Duracion: "+ pelicula.getDuracion());
-        System.out.println("No.Ticket: "+ ticket.getIdticket());
-        System.out.println("Sala: "+ sala.getIdsala());
-        System.out.println("Fecha: "+ reserve.getFechaReserva());
-        
-    }
-    
-    public void imprimirReservaAfiliado(Reserva reserve, Afiliado afiliado, Ticket ticket, Pelicula pelicula,Sala sala){
+	public static Reserva asignarValoresReservasAfiliado() {
+		Reserva reserve = new Reserva();
+		reserve.setCodigoReserva(id++);
+		reserve.setFechaReserva(new Date().toString());
+		return reserve;
+	}
+
+	public Reserva asignarValores() {
+		Reserva reservaNormal = new Reserva();
+		// reservaNormal.setReservaN("1123");
+		reservaNormal.setFechaReserva("1/08/2021");
+
+		return reservaNormal;
+	}
+
+	public void imprimirReservaClienteNormal(Reserva reserve, ClienteNormal clienteNormal, Ticket ticket, Sala sala,
+			Pelicula pelicula) {
+		System.out.println("Reserva Cine");
+		System.out.println("Cliente Nornmal");
+		// System.out.println("RESERVA NO. "+reserve.getReservaN());
+		System.out.println("Nombre Cliente: " + clienteNormal.getNombre());
+		System.out.println("Apellido: " + clienteNormal.getApellido());
+		System.out.println("Edad: " + clienteNormal.getEdad());
+		System.out.println("Precio Normal: " + clienteNormal.getPrecionormal());
+		System.out.println("Pelicula: " + pelicula.getTitulo());
+		System.out.println("Genero: " + pelicula.getGenero());
+		System.out.println("Duracion: " + pelicula.getDuracion());
+		System.out.println("No.Ticket: " + ticket.getIdticket());
+		System.out.println("Sala: " + sala.getIdsala());
+		System.out.println("Fecha: " + reserve.getFechaReserva());
+
+	}
+
+	public void imprimirReservaAfiliado(Reserva reserve, Afiliado afiliado, Ticket ticket, Pelicula pelicula,Sala sala){
 <<<<<<< HEAD
     	if (null == pelicula.getTitulo() && "".equals(pelicula.getTitulo())) {
 	        System.out.println("\n****RESERVA CINE****");
